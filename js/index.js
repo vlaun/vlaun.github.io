d07fa683490dd847e4aa0bda83c42f359aa9377f
Yleana Vlaun <cyvlaun4@gmail.com>
2016-11-10 16:59:31 -0600

    first commit for personal site

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//what I would like to create are 3 separate pages.
+//One page will be the "About Me" that will give the reader a brief description of myself and my
+//experience in software development.
+//The second page will be my "Portfolio" page that will show some of the projects that I have worked on
+//The third page will be my "Resume" page that will allow the reader to access a copy of my resume
+//The last page will be the "Contact Information" page which will provide the reader with my ph#, email and address
+
+//Home Page:  Will have my name and the links to the other pages
+//About Me:  Will give the reader a brief description of myself and my
+//experience in software development.
+//Portfolio:  will show some of the projects that I have worked on
+//Resume:  will allow the reader to access a copy of my resume
+//Contact Information:  will provide the reader with my ph#, email and address
+
